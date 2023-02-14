f0e7830c4f931859e53c20806cdc2b7721be3321
sachindaMass <sachinda46@gmail.com>
2023-02-15 04:08:56 +0530

    project is up on eureka server with error. to resolve the error please check the readme.md

--- src/main/java/com/example/restapi/restapi/RestApiApplication.java
+++ src/main/java/com/example/restapi/restapi/RestApiApplication.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
 public class RestApiApplication implements CommandLineRunner {
 
     private ProductRepository productRepository;
